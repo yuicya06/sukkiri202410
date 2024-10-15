7d416f8ef1852da9d2cdd6c7dd4b3e7c71a343ca
yuicya06 <yuicya1133@gmail.com>
2024-10-15 21:35:44 +0900

    spring

---
+++ SmapleStarts/src/main/java/com/example/demo/SmapleStartsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmapleStartsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmapleStartsApplication.class, args);
+	}
+
+}
